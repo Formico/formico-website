81800479b99cc6b11dc4dc4c77f09d009d1e5a47
Anthony Castrio <anthonycastrio@gmail.com>
2019-10-15 15:07:01 -0400

    remove landing page inspiration code

---
+++ src/components/Team/TeamMember/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import routes from '../../../constants/routes'
+
+import './index.scss'
+
+const TeamMember = ({ name, image, roles }) => (
+  <div className="team-member">
+    <img alt={ `${name}` } src={ `${routes.IMG}/team/${image}` } />
+    <div className="text">
+      <h2 className="name">{ `${name}` }</h2>
+      <p className="roles">{ `${roles}` }</p>
+    </div>
+  </div>
+)
+
+export default TeamMember
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-import FancyBackground from '../components/Index/FancyBackground';
-import Header from '../components/Index/Header';
-import Splash from '../components/Index/Splash';
-import Team from '../components/Index/Team';
-import Process from '../components/Index/Process';
-import Pricing from '../components/Index/Pricing';
-import TechLove from '../components/Index/TechLove';
-import Contact from '../components/Index/Contact';
-import Footer from '../components/Index/Footer';
+import FancyBackground from '../components/FancyBackground';
+import Header from '../components/Header';
+import Splash from '../components/Splash';
+import Team from '../components/Team';
+import Process from '../components/Process';
+import Pricing from '../components/Pricing';
+import TechLove from '../components/TechLove';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import CandyDispenser from '../components/CandyDispenser';
 import Layout from '../layouts';
 
 const IndexPage = ({ ctaUrl }) => (
@@ -22,6 +23,7 @@ const IndexPage = ({ ctaUrl }) => (
       <Pricing />
       <TechLove />
       <Contact />
+      <CandyDispenser />
       <Footer />
     </div>
   </Layout>
