92a7abdfb8102429339ad8ac24eeda9ed3b1f90b
Anthony Castrio <anthonycastrio@gmail.com>
2020-08-01 19:40:59 -0400

    update copyright

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import './index.scss'
 const Header = ({ siteTitle }) => (
   <div className="footer">
     <img alt="" src="/logo/formico-logo.svg" />
-    <p>2018 &copy; Formico</p>
+    <p>2020 &copy; Formico</p>
   </div>
 )
 
