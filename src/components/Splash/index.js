cdd5a398dfd796a4fef19686116ffcb9381ab29b
Anthony Castrio <anthonycastrio@gmail.com>
2020-08-04 18:15:39 -0400

    change Splash copy

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -8,8 +8,9 @@ const Splash = ({ data }) => {
   return (
   <div className="splash">
     <h1 className="tagline">You've Found Your Dev Team</h1>
+    <h2 className="biline">Full stack web-dev: React, Node.js, & Postgres</h2>
     <div className="buttons">
-      <CTA text="Contact Us" link="https://formico.typeform.com/to/CvuKzs" />
+      <CTA text="Get Started" link="https://formico.typeform.com/to/CvuKzs" />
     </div>
   </div>
   )
