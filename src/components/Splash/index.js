115fed366937d2ed981e76063b5470d468222033
Anthony Castrio <anthonycastrio@gmail.com>
2020-08-05 19:28:50 -0400

    rm div that was overlapping cta

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -13,7 +13,6 @@ const Splash = ({ data }) => {
       <div className="buttons">
         <CTA text="Get Started" link="https://formico.typeform.com/to/CvuKzs" />
       </div>
-      <div className="wavy-border" />
     </div>
     {/*<div className="wavy-border"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style={{height: '100%', width: '100%'}}><path d="M0.00,49.98 C151.80,-147.53 306.99,331.08 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style={{stroke: 'none', fill: '#fff'}}></path></svg></div>*/}
   </section>
