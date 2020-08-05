55d7db63aa8f776fdb4cb3a88aec5272a17d5f20
Anthony Castrio <anthonycastrio@gmail.com>
2020-08-05 13:37:31 -0400

    rm tacky typeahead animation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Typing from 'react-typing-animation';
 
 import CTA from '../CTA';
 
@@ -7,27 +6,7 @@ import './index.scss'
 
 const Contact = () => (
   <div className="contact">
-    <h2>
-      <Typing
-        cursorClassName="cursor"
-        speed={ 100 }
-        loop={ true }>
-        Are you ready to...{" "} 
-          <Typing.Delay ms={1000} />
-          build your mvp?
-          <Typing.Delay ms={6000} />
-          <Typing.Backspace count={15} />
-          <Typing.Delay ms={2000} />
-          upgrade your architecture?
-          <Typing.Delay ms={6000} />
-          <Typing.Backspace count={27} />
-          <Typing.Delay ms={2000} />
-          launch your mobile app?
-          <Typing.Delay ms={6000} />
-          <Typing.Backspace count={46} />
-          <Typing.Delay ms={2000} />
-      </Typing>
-    </h2>
+    <h2>{'Get a reply in 24 hours or less.'}</h2>
     <CTA text="Contact Us" link="https://formico.typeform.com/to/CvuKzs" />
   </div>
 )
