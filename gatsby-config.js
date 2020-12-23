078b5bedef466d98dc45313f2bd46950b5970501
Anthony Castrio <anthonycastrio@gmail.com>
2020-12-23 17:46:18 -0500

    remove broken sharp package

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sharp',
     'gatsby-plugin-sass',
-    'gatsby-transformer-sharp',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
